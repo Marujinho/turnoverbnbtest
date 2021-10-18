92a1941bf2191d69726e0a22aa7e93be7c3a796a
Marujinho <aka.contato@gmail.com>
2021-10-17 21:46:41 -0300

    removed a possible feature for lack of time to implement

--- resources/views/components/adminHome.blade.php
+++ resources/views/components/adminHome.blade.php
@@ -1,10 +1,4 @@
 <div>
-    <a href="{{route('transaction.deposit')}}">
-        <button type="button" class="btn btn-primary btn-lg btn-block">SEE CUSTOMERS</button>
-    </a>
-<div>
-<br>
-<div>
     <a href="{{route('transaction.index')}}">
         <button type="button" class="btn btn-primary btn-lg btn-block">DEPOSITS TO BE APPROVED</button>
     </a>
